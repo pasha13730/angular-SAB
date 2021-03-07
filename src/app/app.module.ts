26396a886dd51aadca30f3ebfb570f332d8d4ea7
pasha13730 <ah532703@gmail.com>
2021-03-07 19:36:23 +0530

    4 files modified, 7 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { DesignService } from './service/design.service';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HeaderComponent, SidebarComponent],
+  bootstrap:    [ AppComponent ],
+  providers: [DesignService]
 })
 export class AppModule { }
